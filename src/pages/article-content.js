b4fc68d3b8a37ec218504d06bde6a48a28fff013
dnzengou <desire.yavro@gmail.com>
2020-10-11 12:37:01 +0200

    updated content-pages.js && re-build

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -28,14 +28,84 @@ const articles = [
             etc.). I was also Technical Working Group member for several government’s strategies.`,
         ]
     },     {
-        name: 'my-thoughts',
-        title: 'Entrepreneur, web and business development, GEEKALI Ltd, Rwanda',
+        name: 'my-self-job',
+        title: 'Entrepreneur, web and business development, Geekali Ltd, International',
         content: [
             `Discovering actionable insights to be shared with clients and partners between Europe and Africa, I
             advised on business and technology development accordingly. Applying Lean tools, growth hacking and
             human-centered design. my tech stack includes HTML/CSS/JS and node.js for web development, R on
             RStudio and Python on Jupyter Notebook for data analysis, DevOps from Git to github/ GitLab/ bitbucket
             repositories to the Cloud (Azure, IBM Bluemix, AWS or GP), GIS and CAD with Blender and Unity.`,
+        ]
+    },     {
+        name: 'my-training-other-jobs',
+        title: 'Program Manager, Training Material Specialist, Google, Belgium',
+        content: [
+            `In this fixed-term role, I led the design, delivery and deployment in the EMEA region the first Operational
+            Qualification Standards programme for Operation teams aiming at upscaling staff skills through training 
+            (e-learning and on-the-job). Geolocation specific content and progress tracking ensured via analytics and
+            visualization tools; sites' electrical and mechanical systems mapping designed via open source CAD
+            tools.
+            `,
+        ]
+    },     {
+        name: 'my-data-other-jobs',
+        title: 'Data Analyst, Project Management, International SOS, USA',
+        content: [
+            `Supporting the medical consulting business units in the America’s division with producing business
+            intelligence reports on clients usage of the company’s services, leveraging on specific data processing (R,
+            Python, MS Excel) and visualization tools (GIS - Tableau or Plotly - for mapping.
+            I also explored ways to continuously improve internal reporting processes, supported the pandemic
+            preparedness plans development (Ebola, Influenza) and the division’s data migration to SharePoint.`,
+        ]
+    },     {
+        name: 'my-research-other-jobs',
+        title: 'Research Engineer, Laboratório Regional de Engenharia Civil, Portugal',
+        content: [
+            `Within Seismic Engineering and Metrology unit,
+            - Application of a Non-destructive Testing and Evaluation (NDT&E) technique in Civil Engineering, especially on wooden and concrete structures
+            - Use of FLIR E40bx Camera and Imaging System
+            - Infrared radiation (IR) thermography on Sao Miguel edifices`,
+        ]
+    },     {
+        name: 'my-chemical-other-jobs',
+        title: 'Laboratory Technician - Water Quality, Instituto de Inovação Tecnológica dos Açores, Portugal',
+        content: [
+            `Within Leonardo Da Vinci Program,
+            - Ensure implementation of the Quality Management System for compliance with quality standards (ISO 9377 - for Water quality, and ) and customer needs
+            - Implement on food and water samples analytical methods (Extraction Liquid - Liquid & Gravimetry) for quality control based on the US Environmental Protection Agency (EPA) Method 1664.
+            - Assist in internal quality audits.Ensure implementation of the Quality Management System for compliance with quality standards (ISO 9377 - for Water quality, and ) and customer needs.
+            - Implement analytical methods for food and water (Extraction Liquid - Liquid & Gravimetry) based on US Environmental Protection Agency (EPA) Method 1664 and adapted to site requirements
+            - Assist in regular CIP (Clean In Place) of lab equipements.`,
+        ]
+    },     {
+        name: 'my-learning-other-jobs',
+        title: 'Senior Learning Consultant & Machining Master Trainer, Caterpillar, Belgium',
+        content: [
+            `Working as a Lean Six Sigma ’Champion’ and project coordinator within the Caterpillar Production
+            System (CPS) at its EMEA Master Learning Center, I supported through my role the company’s business
+            and cultural transformation at its multiple facilities via delivery of technical training and operational
+            excellence to machine-operators and assembly cells teams on key fabrication techniques: CNC
+            machining and programming using simulation tools. I also trained operators on Lean manufacturing /
+            Kaizen, Six Sigma for continuous improvement, and Standard Work Environment.`,
+        ]
+    },     {
+        name: 'my-alternative-jobs',
+        title: 'Technologist Trainee, Saint Jean clinical, Belgium',
+        content: [
+            `Within the Radiotherapy department, I ran a Quality Control study intended to explore the link between
+            the exposure index(S) and the dose to patients in the case of the Fujifilm Imaging phosphorus plate.
+            In this framework. I performed the following tasks: establishment of a plan of experiment, ran tests and
+            validation simulating patient's X-ray exam with the use of irradiated ‘phantoms’ of plexiglass.`,
+        ]
+    },     {
+        name: 'my-alternative-other-jobs',
+        title: 'Researcher Trainee, Federal Agency of Nuclear Control, Belgium',
+        content: [
+            `In the Land Surveillance & Natural Ray division, as part of a research project on the mapping of the
+            environmental radioactivity due to radon, I participated in key activities as part of the national Radon plan:
+            field trip for outdoor measurement, radioactivity regional mapping, info sessions to inhabitants for raising
+            awareness. Statistical data analysis, radon mapping using GIS software complement the study.`,
         ]
     },  
 ];
